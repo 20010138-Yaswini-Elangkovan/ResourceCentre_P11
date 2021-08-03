931dfceb266f7579ddbc53651b8cb98fb9a8b09e
20041881 <20041881@LAPTOP-4UF1O56H>
2021-08-03 22:18:30 +0800

    Aini did this

---
+++ ResourceCentre_P11/src/Administrator.java
@@ -0,0 +1,17 @@
+
+public class Administrator {
+	private String Username;
+	private String Password;
+	
+	public Administrator(String Username, String Password) {
+	this.Username = Username;
+	this.Password = Password;
+}
+	public String getUsername() {
+		return Username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+}
+
